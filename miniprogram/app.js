140163b294cc90bf2c180f1944fd33ad818c8877
darkblue5 <xzdo@qq.com>
2020-09-17 11:24:36 +0800

    parse json config data before index.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,15 @@
 //app.js
+import * as config from 'pages/config/config.js';
+
 let _DEBUG_ = true;
+let types = {};
+let grades = [];
 
 App({
     
     onLaunch: function () {
+        let len = config.types.length;
+        let i = 0;
 
         if (!wx.cloud) {
             console.error('请使用 2.2.3 或以上的基础库以使用云能力')
@@ -17,6 +23,11 @@ App({
                 traceUser: true,
             })
         }
+  
+        for(i=0; i<len; i++) {
+            types[config.types[i].grade] = config.types[i].type;
+            grades[i] = config.types[i].grade;
+        }
 
         this.globalData = {
         	openid: '',
@@ -28,6 +39,9 @@ App({
             tdyRate: 0,
             sevenRate: [],  //  本周正确率
             nickName: '佚名',
+            _types: types,
+            _grades: grades
+            
         }
     }
 })
